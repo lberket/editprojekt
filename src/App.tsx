1072be0e5cb11d5a9eb4dd5195acb0b33cb7b740
lberket <lara.berket@gmail.com>
2023-04-22 12:52:54 +0200

    git upitnik

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <h1>Uvod u git</h1>
       <p>Praćenje promjena u repozitoriju</p>
+      <p>PraćUIDZ7UTSZEHUIDJFUzitoriju</p>
+
     </div>
   )
 }
